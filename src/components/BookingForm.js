081e63a43c8e325a0a0fe1700c98f4ebd3d25752
Dionisius Rizky Pramusinto <142286034+rizkydionisius@users.noreply.github.com>
2024-12-02 07:02:23 +0700

    Update BookingForm.js

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,4 +1,3 @@
-// src/components/BookingForm.js
 import React, { useState } from 'react';
 
 const BookingForm = ({ spots, onSpotSelection, onFormSubmit, selectedSpot, errorMessage, bookingSuccess }) => {
